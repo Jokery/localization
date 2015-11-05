c95965c27efbc6b857765503f27fdea760de40fb
Joker <jokerye67@gmail.com>
2015-11-06 01:13:53 +0800

    add trans script

--- scripts/dqm2/trans.rb
+++ scripts/dqm2/trans.rb
@@ -1,24 +1,33 @@
 EXTRA_DIR = File.expand_path('../../_extract_Message_txt', __FILE__)
 TRANS_DIR = File.expand_path('../../_trans_Message_txt', __FILE__)
+DICT_DIR = File.expand_path('../../dict', __FILE__)
 
-puts 123
-puts EXTRA_DIR
-puts 123
 
-def trans_with_desc(dict, file)
+def trans_desc(dict, namefile, descfile)
   hash = get_hash(dict)
-  trans_name = File.open(File.join(TRANS_DIR, "msg_#{file}name.binJ.txt"), 'w')
-  trans_desc = File.open(File.join(TRANS_DIR, "msg_#{file}help.binJ.txt"), 'w')
+  File.open(File.join(TRANS_DIR, "msg_#{descfile}.binJ.txt"), 'w') do |trans_file|
+    names = File.readlines(File.join(EXTRA_DIR, "msg_#{namefile}.binJ.txt"))
+    File.readlines(File.join(EXTRA_DIR, "msg_#{descfile}.binJ.txt")).each_with_index do |line, i|
+      if !( line.count('{') >= 2 ) and !( names[i].count('{') >= 2 )
+        name = names[i][/^[^\{]*/]
+        if ( in_dict = hash[name] )
+          line = in_dict[:desc] + "{TR}\n"
+        end
+      end
+      trans_file.write line
+    end
+  end
 end
 
-def trans_without_desc(dict, file)
+def trans_name(dict, file)
   hash = get_hash(dict)
   File.open(File.join(TRANS_DIR, "msg_#{file}.binJ.txt"), 'w') do |trans_file|
     File.readlines(File.join(EXTRA_DIR, "msg_#{file}.binJ.txt")).each do |line|
-      next if line.count('{') >= 2
-      name = line[/^[^\{]*/]
-      if ( in_dict = hash[name] )
-        line.sub!(name, in_dict[:cn])
+      if not line.count('{') >= 2
+        name = line[/^[^\{]*/]
+        if ( in_dict = hash[name] )
+          line.sub!(name, in_dict[:cn])
+        end
       end
       trans_file.write line
     end
@@ -27,7 +36,7 @@ end
 
 def get_hash(dict)
   hash = { }
-  File.readlines(dict).each do |line|
+  File.readlines(File.join(DICT_DIR, dict)).each do |line|
     jp, cn, desc = line.split(',')
     hash[jp] = { cn: cn, desc: desc }
   end
