a312af41133fae4d2ea304e2e09d967cd6cdf4eb
Joker <jianbo.ye@alo7.com>
2016-04-20 19:02:49 +0800

    update script

--- scripts/dqm2/extract_event.rb
+++ scripts/dqm2/extract_event.rb
@@ -7,9 +7,14 @@ TRAN_SPLITTER = "======================================"
 EVENT_DIR = 'Event'
 EXTRACT_DIR = '_extract_Event_txt'
 
+all_items = { }
+
+
 Dir.glob("#{EVENT_DIR}/**/*.e").each do |fname|
   content = nil
   Zlib::GzipReader.open(fname) { |gz| content = gz.read }
+
+  fname_key = fname.sub("#{EVENT_DIR}/", '')
 
   index = 0
   index2item = { }
@@ -23,15 +28,46 @@ Dir.glob("#{EVENT_DIR}/**/*.e").each do |fname|
     index += length
   end
 
-  File.open(fname.sub(EVENT_DIR, EXTRACT_DIR).sub(/e$/, 'txt'), 'w') do |f|
-    index2item.sort_by(&:first).map(&:last).each_with_index do |item, i|
-      f.write "\nNo.#{i+1}\n"
-      f.write ITEM_SPLITTER + "\n"
-      f.write item + "\n"
-      f.write TRAN_SPLITTER + "\n"
-      f.write item + "\n"
-      f.write ITEM_SPLITTER + "\n"
-      f.write "\n"
+  items = index2item.sort_by(&:first).map(&:last)
+
+  items.each_index do |index|
+    find_index = items.index(items[index])
+    if find_index < index
+      items[index] = "{Duplication #{fname_key} No.#{find_index + 1}}"
+    else
+      other = nil
+      all_items.each do |_fname, _items|
+        _index = _items.index(items[index])
+        if _index
+          other = _fname
+          find_index = _index
+          break
+        end
+      end
+      if other
+        items[index] = "{Duplication #{other} No.#{find_index + 1}}"
+      end
+    end
+  end
+
+  all_items[fname_key] = items
+end
+
+all_items.group_by do |key, items|
+  key[/[^\d]+/]
+end.each do |fname, arr|
+  File.open("#{EXTRACT_DIR}/#{fname}.txt", 'w') do |f|
+    arr.each do |origin_filename, _items|
+      _items.each_with_index do |item, i|
+        f.write "\n#{origin_filename} No.#{i+1}\n"
+        f.write ITEM_SPLITTER + "\n"
+        f.write item + "\n"
+        f.write TRAN_SPLITTER + "\n"
+        f.write item + "\n"
+        f.write ITEM_SPLITTER + "\n"
+        f.write "\n"
+      end
     end
   end
 end
+
