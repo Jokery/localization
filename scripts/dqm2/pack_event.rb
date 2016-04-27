503636e8c35d3c6c52d750dba2d5f3fadb801a18
Joker <jokerye67@gmail.com>
2016-04-27 09:39:13 +0800

    update packevent script

--- scripts/dqm2/pack_event.rb
+++ scripts/dqm2/pack_event.rb
@@ -4,14 +4,20 @@ load File.expand_path('../serializer.rb', __FILE__)
 ITEM_SPLITTER = "--------------------------------------"
 TRAN_SPLITTER = "======================================"
 
+EVENT_DIR = 'Event'
 REPACK_EVENT_DIR = '_repack_Event'
 EXTRACT_DIR = '_extract_Event_txt'
 
 HEADER_SIZE = 4100
+DIALOGUE_FUNC = [
+  "\x85\x00\x00\x00",
+  "\x43\x00\x00\x00",
+]
 SKIP_FUNC = [
-  "\x0c\x04\x00\x00\x0c\x00\x00\x00",
-  "\x09\x04\x00\x00\x0c\x00\x00\x00",
-  "\x0e\x04\x00\x00\x0c\x00\x00\x00"
+  "\x0c\x04\x00\x00", # "\x0c\x00\x00\x00",
+  "\x09\x04\x00\x00", # "\x0c\x00\x00\x00",
+  "\x0e\x04\x00\x00", # "\x0c\x00\x00\x00",
+  "\x0a\x04\x00\x00", # "\x10\x00\x00\x00",
 ]
 
 all_items = { }
@@ -68,88 +74,97 @@ all_items.each_value do |hash|
   end
 end
 
-fname = 'Event/Field/MAA21.e'
-x = all_items['Field/MAA21.e'].sort_by(&:first).map(&:last)
+Dir.glob("#{EVENT_DIR}/**/*.e").each do |fname|
+  x = all_items[fname.sub("#{EVENT_DIR}/", '')]
+  next if x.nil?
+  x = x.sort_by(&:first).map(&:last)
 
-decompress = fname.sub(/\.e$/, '')
-gzfile = decompress + '.gz'
+  decompress = fname.sub(/\.e$/, '')
+  gzfile = decompress + '.gz'
 
-FileUtils.cp(fname, gzfile)
-FileUtils.rm(decompress) if File.exists?(decompress)
+  FileUtils.cp(fname, gzfile)
+  FileUtils.rm(decompress) if File.exists?(decompress)
 
-`gzip -d #{gzfile}`
-content = File.binread(decompress)
+  `gzip -d #{gzfile}`
+  content = File.binread(decompress)
+  FileUtils.rm(decompress)
 
-arr_position2length = [ ]
-arr_pointer2position = [ ]
+  arr_position2length = [ ]
+  arr_pointer2position = [ ]
 
-index = 4
-while index < HEADER_SIZE
-  if ( four = content[index, 4]) != "\xff" * 4
-    point_to = four.unpack('V')[0]
-    arr_pointer2position << [ index, point_to, point_to ]
-  end
-  index += 4
-end
-index = HEADER_SIZE
-index2item = { }
-while ( four = content[index, 4] )
-  index += 4
-  if [ "\x43\x00\x00\x00", "\x85\x00\x00\x00" ].include?(four)
-    length = content[index, 4].unpack('V')[0]
-    arr_position2length << [ index, length ]
+  index = 4
+  while index < HEADER_SIZE
+    if ( four = content[index, 4]) != "\xff" * 4
+      point_to = four.unpack('V')[0]
+      arr_pointer2position << [ index, point_to, point_to ]
+    end
     index += 4
-    item_data = content[index, length - 8]
-    item = BinJSerializer.decode(item_data)
-    index2item[index] = item
-    index += length - 8
-  elsif [ "\x0c\x04\x00\x00", "\x09\x04\x00\x00", "\x0a\x04\x00\x00", "\x0e\x04\x00\x00" ].include?(four)
-    point_to = content[index + 4, 4].unpack('V')[0]
-    arr_pointer2position << [ index + 4, point_to, point_to ]
-    index += 8
   end
-end
-
-items = index2item.sort_by(&:first).map(&:last)
-arr_position2length.sort_by!(&:first)
-
-replaces = [ ]
-
-x.each_index do |index|
-  # next unless index == 39
-  origin = items[index]
-  not_macro_start = 0
-  not_macro_end = -1
-  while origin[not_macro_start] == '{'
-    not_macro_start = origin.index('}', not_macro_start) + 1
-  end
-  while origin[not_macro_end] == '}'
-    not_macro_end = origin.rindex('{', not_macro_end) - 1
-  end
-  x[index] = origin[0...not_macro_start] + x[index] +
-    ( not_macro_end != -1 ?
-    origin[(not_macro_end+1)..-1] :
-    '' )
-  encoded = BinJSerializer.encode(x[index], utf_prefix: "\xe4", appendfix: "\x00", endmacro: "\xe3\x1b")
-  length = encoded.length + 8
-  position2length = arr_position2length[index]
-  origin_position, origin_length = position2length
-  content[origin_position, 4] = [ length ].pack('V')
-  replaces << [ origin_position + 4, origin_length - 8, encoded ]
-  diff = length - origin_length
-  arr_pointer2position.each do |p2p|
-    if ( p2p[2] + HEADER_SIZE ) > origin_position
-      p2p[1] += diff
+  index = HEADER_SIZE
+  index2item = { }
+  while ( four = content[index, 4] )
+    index += 4
+    if DIALOGUE_FUNC.include?(four)
+      length = content[index, 4].unpack('V')[0]
+      arr_position2length << [ index, length ]
+      index += 4
+      item_data = content[index, length - 8]
+      item = BinJSerializer.decode(item_data)
+      index2item[index] = item
+      index += length - 8
+    elsif SKIP_FUNC.include?(four)
+      point_to = content[index + 4, 4].unpack('V')[0]
+      arr_pointer2position << [ index + 4, point_to, point_to ]
+      index += 8
     end
   end
-end
 
-arr_pointer2position.each do |p2p|
-  content[p2p[0], 4] = [ p2p[1] ].pack('V')
-end
+  items = index2item.sort_by(&:first).map(&:last)
+  arr_position2length.sort_by!(&:first)
 
-replaces.reverse_each do |pos, len, encoded|
-  content[pos, len] = encoded
-end
+  replaces = [ ]
 
-File.open('repack_MAA21', 'wb') { |f| f.write content }
+  x.each_index do |index|
+    origin = items[index]
+    not_macro_start = 0
+    not_macro_end = -1
+    while origin[not_macro_start] == '{'
+      not_macro_start = origin.index('}', not_macro_start) + 1
+    end
+    while origin[not_macro_end] == '}'
+      not_macro_end = origin.rindex('{', not_macro_end) - 1
+      break if not_macro_end == -1
+    end
+    x[index] = origin[0...not_macro_start] + x[index] +
+      ( not_macro_end != -1 ?
+       origin[(not_macro_end+1)..-1] :
+       '' )
+      encoded = BinJSerializer.encode(x[index], utf_prefix: "\xe4", appendfix: "\x00", endmacro: "\xe3\x1b")
+      length = encoded.length + 8
+      position2length = arr_position2length[index]
+      origin_position, origin_length = position2length
+      content[origin_position, 4] = [ length ].pack('V')
+      replaces << [ origin_position + 4, origin_length - 8, encoded ]
+      diff = length - origin_length
+      arr_pointer2position.each do |p2p|
+        if ( p2p[2] + HEADER_SIZE ) > origin_position
+          p2p[1] += diff
+        end
+      end
+  end
+
+  arr_pointer2position.each do |p2p|
+    content[p2p[0], 4] = [ p2p[1] ].pack('V')
+  end
+
+  replaces.reverse_each do |pos, len, encoded|
+    content[pos, len] = encoded
+  end
+
+  repack_fname = fname.sub(EVENT_DIR, REPACK_EVENT_DIR)
+  decompress_repack_fname = repack_fname.sub(/\.e$/, '')
+  decompress_gz_fname = decompress_repack_fname + '.gz'
+  File.open(decompress_repack_fname, 'wb') { |f| f.write content }
+  `gzip #{decompress_repack_fname}`
+  FileUtils.mv(decompress_gz_fname, repack_fname)
+end
